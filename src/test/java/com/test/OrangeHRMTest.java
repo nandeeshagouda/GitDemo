586fc1f25718308299964c42956dcbb0e720719d
Nandeesh1 <nandishlk@gmail.com>
2020-12-02 11:02:13 +0530

    frist commit

---
+++ src/test/java/com/test/OrangeHRMTest.java
@@ -0,0 +1,44 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.basePackage.BaseClass;
+
+public class OrangeHRMTest extends BaseClass {
+	
+    @Test
+	public void LoginPageTest() {
+    	test=extent.createTest("LoginPageTest");
+		WebElement imgElement=driver .findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+		Assert.assertTrue(imgElement.isDisplayed());
+	}
+    
+    @Test
+	public void loginTest() {
+    	test=extent.createTest("loginTest");
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		String actualTitle=driver.getTitle();
+		String expectedTitle="OrangeHRM";
+		
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+    
+    @Test
+    public void sampleCase() {
+    	 test=extent.createTest("sampleCase");
+    	  test.createNode("Validation1");
+    	  Assert.assertTrue(true);
+    	  test.createNode("Validation2");
+    	  Assert.assertTrue(true);
+    	  test.createNode("Validation3");
+    	  Assert.assertTrue(true);
+    	  test.createNode("Validation4");
+    	  Assert.assertTrue(true);
+    }
+}
